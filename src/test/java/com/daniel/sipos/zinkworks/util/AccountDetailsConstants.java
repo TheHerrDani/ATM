41a158cb3350df6f496c7bf27a52e26ba3683d31
DESKTOP-BNLR8FO\TheHerrDani <sipos.herr.dani@gmail.com>
2022-07-21 02:35:13 +0200

    Bugfix and minor changes in repository layer

---
+++ src/test/java/com/daniel/sipos/zinkworks/util/AccountDetailsConstants.java
@@ -0,0 +1,35 @@
+package com.daniel.sipos.zinkworks.util;
+
+import com.daniel.sipos.zinkworks.repository.entities.AccountDetails;
+import com.daniel.sipos.zinkworks.service.domain.AccountDetailsDomain;
+
+
+public class AccountDetailsConstants {
+
+  // Suppresses default constructor, ensuring non-instantiability.
+  private AccountDetailsConstants() {
+  }
+
+  public static final long ACCOUNT_DETAILS_ID_ZERO = 0L;
+  public static final long ACCOUNT_DETAILS_ID = 3L;
+  public static final long ACTUAL_BALANCE = 400L;
+  public static final long EIGHT_HUNDRED = 800L;
+  public static final long TWO_HUNDRED = 200L;
+
+
+  public static final AccountDetailsDomain ACCOUNT_DETAILS_DOMAIN = AccountDetailsDomain.builder()
+      .id(ACCOUNT_DETAILS_ID)
+      .actualBalance(ACTUAL_BALANCE)
+      .overdraft(TWO_HUNDRED)
+      .build();
+
+  public static AccountDetails createAccountDetails(long accountDetailsId,
+                                                    long actualBalance,
+                                                    long overdraft) {
+    return AccountDetails.builder()
+        .id(accountDetailsId)
+        .actualBalance(actualBalance)
+        .overdraft(overdraft)
+        .build();
+  }
+}
