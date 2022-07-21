774d4fca4fd041b432a3833e4197ddc1314fe4b1
DESKTOP-BNLR8FO\TheHerrDani <sipos.herr.dani@gmail.com>
2022-07-21 02:31:38 +0200

    Format exceptions

--- src/main/java/com/daniel/sipos/zinkworks/exceptions/AtmMoneyShortageException.java
+++ src/main/java/com/daniel/sipos/zinkworks/exceptions/AtmMoneyShortageException.java
@@ -2,10 +2,8 @@ package com.daniel.sipos.zinkworks.exceptions;
 
 public class AtmMoneyShortageException extends RuntimeException {
 
-
   public AtmMoneyShortageException(String message) {
     super(message);
   }
-
 
 }
--- src/main/java/com/daniel/sipos/zinkworks/exceptions/OverLimitException.java
+++ src/main/java/com/daniel/sipos/zinkworks/exceptions/OverLimitException.java
@@ -2,10 +2,8 @@ package com.daniel.sipos.zinkworks.exceptions;
 
 public class OverLimitException extends RuntimeException {
 
-
   public OverLimitException(String message) {
     super(message);
   }
-
 
 }
